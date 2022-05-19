e2871d23025fda2e8522b17125ebbc9bfcd28387
KalikaSingh11 <495.kalika.singh@gmail.com>
2022-05-19 16:21:44 -0700

    Homework 7 commit

--- blogging/admin.py
+++ blogging/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from blogging.models import Post, Category
 
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ("posts",)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline, ]
+    exclude = ("posts",) 
+
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)  
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)  
