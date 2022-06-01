080083c52423420f0503f44a086d1de186dd1a0d
KalikaSingh11 <495.kalika.singh@gmail.com>
2022-05-31 18:56:12 -0700

    Made some edits to my tests

--- blogging/tests.py
+++ blogging/tests.py
@@ -48,7 +48,7 @@ class FrontEndTestCase(TestCase):
                 post.published_date = pubdate
             post.save()
 
-    def test_list_only_published(self):
+    """def test_list_only_published(self):
         resp = self.client.get("/")
         # the content of the rendered response is always a bytestring
         resp_text = resp.content.decode(resp.charset)
@@ -58,9 +58,9 @@ class FrontEndTestCase(TestCase):
             if count < 6:
                 self.assertContains(resp, title, count=1)
             else:
-                self.assertNotContains(resp, title)
+                self.assertNotContains(resp, title)"""
 
-    """def test_details_only_published(self):
+    def test_details_only_published(self):
         for count in range(1, 11):
             title = "Post %d Title" % count
             post = Post.objects.get(title=title)
@@ -69,4 +69,4 @@ class FrontEndTestCase(TestCase):
                 self.assertEqual(resp.status_code, 200)
                 self.assertContains(resp, title)
             else:
-                self.assertEqual(resp.status_code, 404)"""
+                self.assertEqual(resp.status_code, 404)
