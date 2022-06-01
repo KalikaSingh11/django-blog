fab2ab0be7363cb02ac79edbf49eadbb77a03b39
KalikaSingh11 <495.kalika.singh@gmail.com>
2022-06-01 14:57:19 -0700

    Ran black on blogging/views.py

--- blogging/views.py
+++ blogging/views.py
@@ -23,14 +23,16 @@ def stub_view(request, *args, **kwargs):
 
 class PostListView(ListView):
     queryset = Post.objects.exclude(published_date__exact=None).order_by(
-        "-published_date")
+        "-published_date"
+    )
     template_name = "list.html"
 
 
 class PostDetailView(DetailView):
     template_name = "blogging/detail.html"
     queryset = Post.objects.exclude(published_date__exact=None).order_by(
-        "-published_date")
+        "-published_date"
+    )
 
     def post(self, request, *args, **kwargs):
         try:
