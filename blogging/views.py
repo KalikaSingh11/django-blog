f26bafbc92ee7fb8d918d465a31263919401e4fe
KalikaSingh11 <495.kalika.singh@gmail.com>
2022-06-02 17:18:08 -0700

    Intial commit for homework 9 - chose to do addon 3

--- blogging/views.py
+++ blogging/views.py
@@ -5,9 +5,38 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.http import Http404
 
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
+from blogging.serializers import UserSerializer, PostSerializer, CategorySerializer
+from blogging.models import Post, Category
 
 # Create your views here..
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 def stub_view(request, *args, **kwargs):
